c247c86d1ea8a69ec862ec53b990a6cc89b63ee5
Sam Potter <31372513+stpotter16@users.noreply.github.com>
2018-10-22 13:23:48 -0500

    Initial committ of fileIO module

---
+++ fileIO.py
@@ -0,0 +1,9 @@
+"""
+.. module:: fileIO
+    :platform: Unix, Windows
+    :synopsis: Methods for reading and writing to files (image data, strain results)
+
+.. moduleauthor:: Sam Potter <spotter1642@gmail.com>
+"""
+
+from . import np
