fbefecff4c14f459e54e68f7b2389d6ce3ebddc9
Sam Potter <31372513+stpotter16@users.noreply.github.com>
2018-11-05 15:35:32 -0600

    Adding tiff stack writing function to fileIO module

--- fileIO.py
+++ fileIO.py
@@ -10,6 +10,9 @@ from . import np
 from . import sio
 from . import os
 from . import cv2
+from . import plt
+from . import cm
+from . import utilities
 
 
 def read_mat_file(fname):
@@ -76,10 +79,69 @@ def read_tiff_stack(directory, read_mode=-1):
     return image_dict
 
 
-def write_gray_tiff_stack(image_array, base_name, directory=None):
+def write_tiff_stack(data_array, base_name, directory=None, color_map='viridis'):
 
     """
-    Writes a 3 dimensional array of data to 16 bit gray scale tiff image stacks.
-    :param image_array:
-    :return:
+    Writes a 3 dimensional array of data to 16 bit tiff image stacks.
+    :param data_array: Data array to write to image. Shape N x Image X Size x Image Y Size. Assumed that input is not
+    normalized.
+    :type data_array: numpy array
+    :param base_name: base name string of output file. Will be appended with image index number and .tiff extension
+    :type base_name: str
+    :param directory: Optional. Full path to write output files into. Default will be current working directory of
+    script
+    :type directory: str
+    :param color_map: Optional. Matplotlib colormap to use for image write.
+    :type color_map: str
+    :return: None
+    :rtype: None
     """
+    # Input checking
+    if len(data_array.shape) != 3:
+        raise ValueError('Data array must be three dimensional!')
+
+    if not isinstance(base_name, str):
+        raise TypeError('Base name for image output must be a string')
+
+    # Check if directory needs to be changes
+    if directory:
+        # Check input
+        if not isinstance(directory, str):
+            raise ValueError('Output directory path must be a string!')
+
+        # Save current directory for later use
+        start = os.getcwd()
+
+        # Change directory
+        os.chdir(directory)
+
+    # Loop through every image in array and write output image
+    for i in range(len(data_array)):
+
+        # Normalize input data to [0, 1]
+        utilities.normalize_2d(data_array[i, :, :])
+
+        # Get colormap
+        colormap = cm.get_cmap(color_map)
+
+        # Apply colormap to data
+        im_data = colormap(data_array[i, :, :])
+
+        # Scale to 16 bit range
+        im_data = 65535 * im_data
+
+        # Convert to unsigned 16 int type
+        im_data = np.uint16(im_data)
+
+        # Flip RGB order to BGR order so it's compatible with OpenCV
+        im_data = im_data[..., ::-1]
+
+        # Create file output name
+        fname = base_name + '_' + str(i) + '.tiff'
+
+        # Write file
+        cv2.imwrite(fname, im_data)
+
+    if directory:
+        # Move directory back to start
+        os.chdir(start)
