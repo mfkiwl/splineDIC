983841aaaa7be540ad23d746eb8682fcf909c276
Sam Potter <31372513+stpotter16@users.noreply.github.com>
2018-11-06 14:16:34 -0600

    Adding numerics package

---
+++ numerics.py
@@ -0,0 +1,23 @@
+"""
+.. module:: numerics
+    :platform: Unix, Windows
+    :synopsis: Module contains functions for basic numerical manipulations of image data, e.g.
+    bicubic interpolation, multigrid dimension image resizing, quadrature, masking, etc.
+
+.. moduleauthor:: Sam Potter <spotter1642@gmail.com>
+"""
+
+from . import np
+
+
+def image_interp(im_data, degree='cubic'):
+
+    """
+    Doc string
+    :param im_data:
+    :param degree:
+    :return:
+    """
+    # Compute and return interpolation
+    pass
+
