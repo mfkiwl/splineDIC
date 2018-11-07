fb6eb41234023f0a34ede515d194494c39504e67
Sam Potter <31372513+stpotter16@users.noreply.github.com>
2018-11-06 19:00:41 -0600

    Adding a new module image.py containing dic image class definitions

---
+++ image.py
@@ -0,0 +1,8 @@
+"""
+.. module:: image
+    :platform: Unix, Windows
+    :synopsis: Module contains class definitions and associated attributes/methods for DIC image objects
+
+.. moduleauthor:: Sam Potter <spotter1642@gmail.com>
+"""
+
