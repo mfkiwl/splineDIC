63336dbd9d646f55e9a998b06820dc1a0845d0e3
Sam Potter <31372513+stpotter16@users.noreply.github.com>
2018-11-06 20:06:05 -0600

    Adding data propety and getter/setter to image class

--- image.py
+++ image.py
@@ -6,3 +6,55 @@
 .. moduleauthor:: Sam Potter <spotter1642@gmail.com>
 """
 
+from . import np
+
+
+class Image:
+
+    """
+    Data storage and evaluation class for DIC image data
+
+    Attributes
+    data: discrete image data values at each pixel level. Shape Size X x Size Y.
+
+    Methods
+    """
+
+    def __init__(self, data):
+        self._data = data
+
+    # Data property definition
+    @property
+    def data(self):
+        """
+        Image data
+        2D Numpy array. Shape Size X x Size Y.
+
+        :getter: Get image data
+        :setter: Set image data
+        :type: ndarray
+        """
+        return self._data
+
+    # Data property getter
+    @data.setter
+    def data(self, array):
+        """
+        Sets image data and performs type checking
+
+        Provides a consistent way to set the imaging data and directly operates on the class attribute _data
+        :param array: input data. Numpy array. Shape Size X x Size Y
+        :type array: ndarray
+        :return: None
+        :rtype: None
+        """
+        # Input checking: Numpy array
+        if not isinstance(array, np.ndarray):
+            raise TypeError('Data must be a 2D Numpy Array')
+
+        # Input checking: 2D array
+        if array.ndim != 2:
+            raise ValueError('Data shape must be 2D')
+
+        # Set _data property
+        self._data = array
