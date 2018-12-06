51c6da79d23bc51d54760b2dcc62b9d41aa60478
Samuel Potter <spotter@myocomp.ices.utexas.edu>
2018-12-06 09:43:19 -0600

    adding 1d array normalization

--- dic/utilities.py
+++ dic/utilities.py
@@ -31,3 +31,28 @@ def normalize_2d(data_array):
     # Scale values to range [0, 1]
     data_array = data_array - data_min * np.ones(data_array.shape)  # Subtract off min value
     data_array *= 1/(data_max - data_min)  # Multiply by scaling term
+
+
+
+# Normalize 1D array
+def normalize_1d(data_array):
+    
+    """
+    Normalize a 1D data array to range [0, 1].
+    
+    :param data_array: Input 1d data
+    :type data_array: ndarray
+    :return: None
+    :rtype: None
+    """
+    # Sanitize input
+    if data_array.ndim != 1:
+        raise ValueError('Input data must be 1D!')
+    
+    data_max = np.max(data_array)
+    data_min = np.min(data_array)
+    
+    data_array = data_array - data_min * np.ones(data_array.shape)
+    data_array *= 1/(data_max - data_min)
+    
+    return data_array
