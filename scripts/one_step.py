cc5cc05760a711e66bd26669e1d79c5e2054aa2d
Sam Potter <spotter1642@gmail.com>
2018-12-19 15:50:39 -0600

    script header info

--- scripts/one_step.py
+++ scripts/one_step.py
@@ -1,3 +1,7 @@
 '''
-basic
+.. script:: one_step
+    :platform: Unix, Windows
+    :synopsis: Compute a NURBS DIC analysis between two images in single step
+
+.. scriptauthor:: Sam Potter <spotter1642@gmail.com>
 '''
