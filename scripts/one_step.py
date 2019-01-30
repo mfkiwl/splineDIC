077f2525695d504726ade556f65517ea4a9848da
Sam Potter <spotter1642@gmail.com>
2019-01-30 06:43:32 -0600

    updating initial condition and adding some extra outputs

--- scripts/one_step.py
+++ scripts/one_step.py
@@ -108,12 +108,15 @@ def minfun_nm(disp_vec, *args):
 # Setup initial displacement vector
 int_disp_vec = np.zeros(2*len(coords))
 for i in range(0, len(int_disp_vec), 2):
-    int_disp_vec[i] = 4.0
+    int_disp_vec[i] = 5.0
     int_disp_vec[i+1] = 0.0
 
 print('Begin minimization')
 pr.enable()
 result = sciopt.minimize(minfun_nm, int_disp_vec, args=arg_tup, method='Nelder-Mead', options={'maxiter': 10, 'disp': True})
+print('residual')
+print(results.fun)
+print('final control point displacements'
 print(result.x)
 pr.disable()
 pr.dump_stats('opt.pstat')
