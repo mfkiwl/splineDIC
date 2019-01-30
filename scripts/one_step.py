19351e580fd91738ba40a887658165ff75f9f869
Sam Potter <spotter1642@gmail.com>
2019-01-30 11:02:08 -0600

    adding new notebook and small modifications to testing scripts

--- scripts/one_step.py
+++ scripts/one_step.py
@@ -110,7 +110,7 @@ def minfun_nm(disp_vec, *args):
 # Setup initial displacement vector
 int_disp_vec = np.zeros(2*len(coords))
 for i in range(0, len(int_disp_vec), 2):
-    int_disp_vec[i] = 4.0
+    int_disp_vec[i] = 5.0
     int_disp_vec[i+1] = 0.0
 
 print('Actual Rigid X Displacement: {}'.format(dx))
