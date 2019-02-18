c235bfe1a29e813cf9e6aeda976e2a436c551113
Sam Potter <spotter1642@gmail.com>
2019-02-18 07:22:33 -0600

    updating

--- scripts/synth_def.py
+++ scripts/synth_def.py
@@ -10,6 +10,7 @@
 # bootstrap $PATH
 import sys
 import os
+
 sys.path.extend(['C:\\Users\\potterst1\\Desktop\Repositories\BitBucket\dic',
                  'C:/Users/potterst1/Desktop/Repositories/BitBucket/dic'])
 sys.path.extend(['/workspace/stpotter/git/bitbucket/dic'])
@@ -19,6 +20,7 @@ from dic import fileIO
 from dic import numerics
 from dic import analysis
 from dic import image_processing
+from dic import visualize
 import cv2
 from matplotlib import pyplot as plt
 import numpy as np
@@ -35,22 +37,22 @@ pr.disable()
 
 # Parse input
 try:
-	name = sys.argv[1]
-	dx = float(sys.argv[2])
-	dy = float(sys.argv[3])
-	F11 = float(sys.argv[4])
-	F12 = float(sys.argv[5])
-	F21 = float(sys.argv[6])
-	F22 = float(sys.argv[7])
-except:
-	print('Invalid command line arguments')
-	sys.exit(1)
+    name = sys.argv[1]
+    dx = float(sys.argv[2])
+    dy = float(sys.argv[3])
+    F11 = float(sys.argv[4])
+    F12 = float(sys.argv[5])
+    F21 = float(sys.argv[6])
+    F22 = float(sys.argv[7])
+except IndexError:
+    print('Invalid command line arguments')
+    sys.exit(1)
 
 # Change to output directory
 start = os.getcwd()
 try:
     os.chdir(name)
-except:
+except OSError:
     os.makedirs(name)
     os.chdir(name)
 
@@ -62,14 +64,14 @@ def_image = cv2.imread(dic_name, -1)  # Read in image 'as is'
 
 # Translate image
 F = np.array([[F11, F12],
-            [F21, F22]])
+              [F21, F22]])
 Finv = np.linalg.inv(F)
 F11i = Finv[0, 0]
 F12i = Finv[0, 1]
 F21i = Finv[1, 0]
 F22i = Finv[1, 1]
 warp = np.array([[F11i, F12i, -dx],
-                   [F21i, F22i, -dy]])
+                 [F21i, F22i, -dy]])
 ref_image = image_processing.im_warp(def_image, warp)
 
 # Specify region of interest
@@ -105,9 +107,6 @@ ref_surf.knotvector_v = gutil.generate_knot_vector(ref_surf.degree_v, num_ctrlpt
 
 ref_surf.delta = 0.01
 
-# Wrap minimization arguments into a tuple
-arg_tup = (ref_image, def_image, ref_surf)
-
 # Plot image with reference mesh nodes
 x = coords[:, 0]
 y = coords[:, 1]
@@ -127,89 +126,83 @@ synth_coords_disp = synth_coords - coords
 # Compute znssd between synthetic and ref coordinates
 synth_znssd = analysis.mesh_znssd(ref_image, def_image, ref_surf, synth_coords_disp)
 
-def minfun_nm(disp_vec, *args):
-    '''
-    Minimizatin function for passing to scipy minimize using Nelder-Mead method
+# Print the synthetic info to stdout
+print('Synthetic ZNSSD: {}'.format(synth_znssd))
+print('Synthetic Coordinate Displacements')
+print(synth_coords_disp)
 
-    Assembles solution vector and arguments, then passes to mesh_znssd to compute cost
+# Visualize synthetic displacement results
+# Set up new surface
+disp_surf = bs.Surface()
 
-    :param disp_vec: trial displacement vector. Shape is (1, 2*num_ctrltps) and is orderd
-    [delta x0, delta y0, delta x1, delta y1, etc.]
-    :type disp_vec: ndarray
-    return: scalar value of mesh znssd at the trial displacement vector
-    :rtype: float
-    '''
+disp_surf.degree_u = 3
+disp_surf.degree_v = 3
 
-    # Assemble displacement vector
-    ctrlpt_disp = np.zeros((int(len(disp_vec) / 2), 2))
-    for i in range(0, len(disp_vec), 2):
-        k = i // 2  # Modulo to keep the index from over running length of ctrlpt_disp
-        ctrlpt_disp[k, :] = np.array([disp_vec[i], disp_vec[i + 1]])
+disp_surf.set_ctrlpts(synth_coords_disp.tolist(), num_ctrlpts, num_ctrlpts)
 
-    # Call znssd with defaults on all keyward params. This will make it a bit slower, but whatever for now
-    znssd = analysis.mesh_znssd(*args, ctrlpt_disp)
+disp_surf.knotvector_u = gutil.generate_knot_vector(disp_surf.degree_u, num_ctrlpts)
+disp_surf.knotvector_v = gutil.generate_knot_vector(disp_surf.degree_v, num_ctrlpts)
 
-    return znssd
+disp_surf.delta = 0.01
+visualize.viz_displacement(ref_image, disp_surf, rowmin, rowmax, colmin, colmax, name + 'SynthDisplacement.png')
 
-# Compute rigid initial correlation with 2d correlation
-rowmid = int(np.mean([rowmin, rowmax]))
-colmid = int(np.mean([colmin, colmax]))
+# Visualize synthetic deformation results
+visualize.viz_deformation(ref_image, ref_surf, rowmin, rowmax, colmin, colmax, synth_coords_disp,
+                          name + 'SynthDeformation.png')
 
-# Get subimage
-subimage = np.copy(ref_image[subregion_indices[2]:subregion_indices[3], subregion_indices[0]:subregion_indices[1]])
+# Wrap minimization arguments into a tuple
+arg_tup = (ref_image, def_image, ref_surf)
 
-# Normalize images
-ref_subnorm = subimage - subimage.mean()
-def_norm = def_image - def_image.mean()
-
-# Correlate
-corr = sig.correlate2d(def_norm, ref_subnorm, boundary='symm', mode='same')
-midy, midx = np.unravel_index(np.argmax(corr), corr.shape)
-
-initx = (midx + 1) - colmid
-inity = (midy + 1) - rowmid
-
-# Setup initial displacement vector
-int_disp_vec = np.zeros(2*len(coords))
-for i in range(0, len(int_disp_vec), 2):
-    int_disp_vec[i] = initx
-    int_disp_vec[i+1] = inity
 
 # compute mesh znssd one time and exit if its low enough
+int_disp_vec = analysis.rigid_guess(ref_image, def_image, rowmin, rowmax, colmin, colmax, len(coords))
 
-pr.enable()
+# compute mesh znssd one time and exit if its low enough
+#pr.enable()
 
-residual = minfun_nm(int_disp_vec, *arg_tup)
+residual = analysis.scipy_minfun(int_disp_vec, *arg_tup)
 
 if residual > 1e-6:
     print('Begin minimization')
-    if method == 'L-BFGS-B':
-        result = sciopt.minimize(minfun_nm, int_disp_vec, args=arg_tup, method='L-BFGS-B', jac='2-point', bounds=None, options={'disp': True})
-    elif method == 'Nelder-Mead':
-        result = sciopt.minimize(minfun_nm, int_disp_vec, args=arg_tup, method='Nelder-Mead', options={'disp': True})
-    else:
-        result = sciopt.minimize(minfun_nm, int_disp_vec, args=arg_tup, method='BFGS', jac='2-point', options={'disp': True})
+    result = sciopt.minimize(analysis.scipy_minfun, int_disp_vec, args=arg_tup, method='L-BFGS-B', jac='2-point', bounds=None, options={'maxiter': 10, 'disp': True})
 
-print('Prescribed Rigid X Displacement: {}'.format(dx))
-print('Prescribed Rigid Y Displacement: {}'.format(dy))
-print('Prescribed F11 F12 F21 F22: {} {} {} {}'.format(F11, F12, F21, F22))
+print('Actual Rigid X Displacement: {}'.format(dx))
+print('Actual Rigid Y Displacement: {}'.format(dy))
 print('Mesh Details: {} by {}'.format(num_ctrlpts, num_ctrlpts))
-print('ROI Size: {} by {}'.format(rowmax-rowmin, colmax - colmin))
-print('Initial Guess -  X Displacement: {}'.format(initx))
-print('Initial Guess - Y Displacement: {}'.format(inity))
+print('Initial Guess -  X Displacement: {}'.format(int_disp_vec[0]))
+print('Initial Guess - Y Displacement: {}'.format(int_disp_vec[1]))
 
 if residual > 1e-6:
     print('residual')
     print(result.fun)
+    coords_disp = np.column_stack((result.x[::2], result.x[1::2]))
     print('final control point displacements')
-    print(result.x)
+    print(coords_disp)
 else:
     print('residual')
     print(residual)
+    coords_disp = np.column_stack((int_disp_vec[::2], int_disp_vec[1::2]))
     print('final control point displacement')
-    print(int_disp_vec)
+    print(coords_disp)
 
-pr.disable()
-pr.dump_stats('opt.pstat')
+# Visualize minimization result displacement
+# Set up new surface
+disp_surf = bs.Surface()
+
+disp_surf.degree_u = 3
+disp_surf.degree_v = 3
+
+disp_surf.set_ctrlpts(coords_disp.tolist(), num_ctrlpts, num_ctrlpts)
+
+disp_surf.knotvector_u = gutil.generate_knot_vector(disp_surf.degree_u, num_ctrlpts)
+disp_surf.knotvector_v = gutil.generate_knot_vector(disp_surf.degree_v, num_ctrlpts)
+
+disp_surf.delta = 0.01
+
+visualize.viz_displacement(ref_image, disp_surf, rowmin, rowmax, colmin, colmax, name + 'MinDisplacement.png')
+# Visualize minimization result deformation
+visualize.viz_deformation(ref_image, ref_surf, rowmin, rowmax, colmin, colmax, coords_disp, name + 'MinDeformation.png')
+
+#pr.disable()
+#pr.dump_stats('opt.pstat')
 print('execution time (s)')
-
