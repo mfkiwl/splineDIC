0a01ae3865b4bc7ffbd36ef84b4ff67ecbe323cb
Sam Potter <spotter1642@gmail.com>
2019-02-27 16:29:41 -0600

    adding profiler

--- scripts/mesh_znssd_eval.py
+++ scripts/mesh_znssd_eval.py
@@ -183,8 +183,12 @@ for i in range(len(synth_coords)):
 synth_coords_disp = synth_coords - coords
 
 # Compute znssd between synthetic and ref coordinates
+pr.enable()
 synth_znssd = analysis.mesh_znssd_bicubic(roi, ref_sub_image.shape, def_sub_image.shape, ref_surf, uv_vals,
                                           ref_sub_coeff, def_sub_coeff, synth_coords_disp)
 
-print(synth_znssd)
+pr.disable()
+pr.dump_stats('mesh_eval_znssd.pstat')
+
+print('ZNSSD: {}'.format(synth_znssd))
 print('Execution time:')
