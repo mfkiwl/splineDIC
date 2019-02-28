4f4ea1d5c769e24b9ed33e6a0822010d9b76aad2
Sam Potter <spotter1642@gmail.com>
2019-02-28 15:28:46 -0600

    removing b-spline interpolation and znssd algorithms. abadoning that direction for now

--- dic/analysis.py
+++ dic/analysis.py
@@ -18,7 +18,7 @@ def discrete_znssd(ref_image, def_image):
     """
     Compute the zero normalized sum of square differences (ZNSSD) between two image subsets.
     See Pan et al. Meas Sci Tech 2009 for details
-    
+
     :param ref_image: reference image.
     :type ref_image: ndarray
     :param def_image: deformed image.
@@ -96,7 +96,6 @@ def mesh(ref_cpts, degree=3):
     surf.delta = 0.001
 
     return surf
-
 
 
 # TODO: Rewrite this and just use projection and Newton's methods
@@ -288,6 +287,7 @@ def mesh_znssd_bicubic(roi, ref_shape, def_shape, ref_mesh, uv_vals, ref_coeff, 
     rowmax = roi.shape[0]
     colmax = roi.shape[1]
 
+    # TODO: Vectorize this?
     for i in range(0, rowmax):
         for j in range(0, colmax):
             u_val = uv_vals[0, i, j]
@@ -340,144 +340,6 @@ def scipy_minfun_bicubic(disp_vec, *args):
     return znssd
 
 
-def mesh_znssd_spline(ref_image, def_image, ref_mesh, uv_vals, ref_coeff, def_coeff, interp_order, cpts_disp):
-    """
-    Compute the zero normalized sum of square differences over an entire mesh between two images using spline
-    interpolation
-    See Pan et al. Meas Sci Tech 2009 for details.
-
-    :param ref_image: reference image
-    :type ref_image: ndarray
-    :param def_image: deformed image
-    :type def_image: ndarray
-    :param ref_mesh: reference state B-spline mesh
-    :type ref_mesh: Bspline surface
-    :param uv_vals: Array containing u, v parameterization of the pixels in the reference image
-    :type uv_vals: ndarray
-    :param ref_coeff: 2D array of b-spline coefficients for reference image
-    :type ref_coeff: ndarray
-    :param def_coeff: 2D array of b-spline coefficients for deformed image
-    :type def_coeff: ndarray
-    :param interp_order: order of b-spline interpolation. Options: {'cubic', 'quintic'}
-    :type interp_order: str
-    :param cpts_disp: control point displacements [delta X, delta Y]
-    :type cpts_disp: ndarray
-    :return: ZNSSD between the two images in the mesh region
-    :rtyp: float
-    """
-
-    # Sanitize input
-    if ref_image.ndim != 2:
-        raise ValueError('Reference image input must be 2d')
-
-    if def_image.ndim != 2:
-        raise ValueError('Deformed image input must be 2d')
-    
-    # Check if reference image interpolation coefficients are correctly sized
-    if not ref_coeff.shape == ref_image.shape:
-        raise ValueError('Coefficient array and image array must have same dimension (reference image)')
-    
-    # Check if deformed image interpolation coefficients are correctly sized
-    if not def_coeff.shape == def_image.shape:
-        raise ValueError('Coefficient array and image array must have same dimension (deformed image)')
-
-    if ref_image.shape != def_image.shape:
-        raise ValueError('Images must be the same size')
-
-    # Get ref mesh control points
-    ref_cpts = np.array(ref_mesh.ctrlpts)
-
-    # Get def mesh 
-    def_mesh = deform_mesh(ref_mesh, cpts_disp)
-
-    # Get min and max column values from min/max reference ctrlpt node x values
-    min_col_index = np.min(ref_cpts[:, 0]).astype('int')
-    max_col_index = np.max(ref_cpts[:, 0]).astype('int')
-
-    # Get maximum column number for sub image array from ref ctrlpt node x values
-    colmax = (np.max(ref_cpts[:, 0]) - np.min(ref_cpts[:, 0])).astype('int')
-
-    # Get min and max column values from min/max reference ctrlpt node x values
-    min_row_index = np.min(ref_cpts[:, 1]).astype('int')
-    max_row_index = np.max(ref_cpts[:, 1]).astype('int')
-
-    # Get min and max row values from min/max reference ctrlpt node y values
-    rowmax = (np.max(ref_cpts[:, 1]) - np.min(ref_cpts[:, 1])).astype('int')
-
-    # Set reference image mesh over image
-    f_mesh = ref_image[min_row_index:max_row_index, min_col_index:max_col_index]
-
-    # Compute mean of this reference image mesh
-    fmean = np.mean(f_mesh)
-
-    # Compute standard deviation this reference image mesh
-    fstddev = np.std(f_mesh)
-
-    # For every pixel in f, compute the new location in g
-    g_mesh = np.zeros(f_mesh.shape)
-
-    # Check uv_vals have same shape as ref image
-    if not uv_vals.shape == ref_image.shape:
-        raise ValueError('u, v parameterization array must be same shape as reference image')
-
-    for i in range(0, rowmax):
-        for j in range(0, colmax):
-            u_val = uv_vals[0, i, j]
-            v_val = uv_vals[1, i, j]
-
-            # Compute the displacement by interpolating
-            new_pt = def_mesh.surfpt(u_val, v_val)
-
-            g_mesh[i, j] = numerics.eval_interp_spline(new_pt[0], new_pt[1], def_image, coeffs=def_coeff,
-                                                       order=interp_order)
-
-    # Compute mean of this deformed image mesh
-    gmean = np.mean(g_mesh)
-
-    # Compute standard deviation of this deformed image mesh
-    gstddev = np.std(g_mesh)
-
-    # Loop over these matrices and compute ZNSSD (could be much faster)
-    znssd = 0.0
-    '''
-    for i in range(0, rowmax):
-        for j in range(0, colmax):
-            znssd += np.square((f_mesh[i, j] - fmean) / fstddev - (g_mesh[i, j] - gmean) / gstddev)
-    '''
-    fval = 1 / fstddev * (f_mesh - np.ones(f_mesh.shape) * fmean)
-    gval = 1 / gstddev * (g_mesh - np.ones(g_mesh.shape) * gmean)
-
-    znssd = np.square(fval, gval)
-
-    return znssd
-
-
-def scipy_minfun_spline(disp_vec, *args):
-
-    '''
-    Minimization function for passing to scipy minimize
-
-    Assembles solution vector and arguments, then passes to mesh_znssd_spline to compute cost
-
-    :param disp_vec: trial displacement vector. Shape is (1, 2*number of mesh control points
-    order is [delta x0, delta y0, delta x1, delta y1, etc.]
-    :type disp_vec: ndarray
-    :return: scalar value of mesh znssed at the trial displacement vector
-    :rtype: float
-    '''
-
-    # Assemble displacement vector
-    ctrlpt_disp = np.zeros((int(len(disp_vec) / 2), 2))
-    for i in range(0, len(disp_vec), 2):
-        k = i // 2  # Module to keep the index from over running lenght of control points
-        ctrlpt_disp[k, :] = np.array([disp_vec[i], disp_vec[i + 1]])
-
-    # Call znssd with defaults on all keyword params. This is slow, but okay for now
-    znssd = mesh_znssd_spline(*args, ctrlpt_disp)
-
-    return znssd
-
-
 def rigid_guess(ref_image, def_image, rowmin, rowmax, colmin, colmax, num_cpts):
 
     """
@@ -526,80 +388,3 @@ def rigid_guess(ref_image, def_image, rowmin, rowmax, colmin, colmax, num_cpts):
 
     return int_disp_vec
 
-
-def minfun(delta, nodes_ref, ref_im, def_im):
-    """
-    Function for scipy minimizer to minimize
-
-    :param delta: 1D arrary of rigid body rotations
-    :type delta: ndarray
-    :return: ZNSSD of deformed ref and deformed image
-    :rtype: float
-    """
-    # TODO: Type checks
-
-    # Get deltas
-    dx = delta[0]
-    dy = delta[1]
-
-    # Deform the nodes
-    # Copy and update reference image locations only
-    nodes_def = np.copy(nodes_ref)  # Copy
-    nodes_def[:, 0] += dx * np.ones(len(nodes_def))
-    nodes_def[:, 1] += dy * np.ones(len(nodes_def))
-
-    # Min/max nodes in x/y for ref and def
-    # TODO: Refactor
-    # Round values down as the step size must be interger valued
-    minx_ref = np.min(nodes_ref[:, 0]).astype('int')
-    miny_ref = np.min(nodes_ref[:, 1]).astype('int')
-    maxx_ref = np.max(nodes_ref[:, 0]).astype('int')
-    maxy_ref = np.max(nodes_ref[:, 1]).astype('int')
-
-    minx_def = np.min(nodes_def[:, 0]).astype('int')
-    miny_def = np.min(nodes_def[:, 1]).astype('int')
-    maxx_def = np.max(nodes_def[:, 0]).astype('int')
-    maxy_def = np.max(nodes_def[:, 1]).astype('int')
-
-    # Get ref and def images
-    ref_subset = ref_im[miny_ref:maxy_ref, minx_ref:maxx_ref]
-    def_subset = def_im[miny_def:maxy_def, minx_def:maxx_def]
-
-    # Compute ZNSSD
-    znssd = discrete_znssd(ref_subset, def_subset)
-
-    return znssd
-
-
-def ratchet(maxdx, maxdy, nodes_ref, ref_image, def_image):
-    """
-    Compute rigid deformation by ratcheting window over image
-
-    :param maxdx: maximum step in x
-    :type maxdx: int
-    :param maxdy: maximum step in y
-    :type maxdy: int
-    :param ref_image: reference image array
-    :type ref_image: ndarray
-    :param def_image: deformed image array
-    :type def_image: ndarray
-    :return: displacement [dx dy]
-    :rtype:ndarray
-    """
-
-    minval = 1000
-
-    for i in range(0, maxdx):
-        dx = i
-        for j in range(0, maxdy):
-            dy = j
-            delta = np.array([dx, dy])
-            retval = minfun(delta, nodes_ref, ref_image, def_image)
-            if retval < minval:
-                minval = retval
-            if np.isclose(minval, 0.0):
-                return np.array([dx, dy, minval])
-
-    warnings.warn('Could not find exact minimum value')
-
-    return np.array([dx, dy, minval])
