b20955f30a176ea2b87b0fa51ba64637c395ea8a
Sam Potter <spotter1642@gmail.com>
2019-03-27 10:42:17 -0500

    adding basis function

--- src/nurbs.c
+++ src/nurbs.c
@@ -8,3 +8,30 @@
  *-------------------------------------------------------------------------------------*/
  
  #include nurbs.h
+ 
+ double basis_functions(int knot_span, double knot, int degree, double knot_vector)
+ {
+	 double N[degree + 1] = { 1 };
+	 double left[degree + 1] = { 0 };
+	 double right[degree + 1] = { 0 };
+	 double saved;
+	 double temp;
+	 
+	 int j;
+	 for(j=1; j<=degree; j++){
+		 left[j] = knot - knot_vector[knot_span + 1 - j];
+		 right[j] knot_vector[knot_span + j] - knot;
+		 saved = 0.0;
+		 
+		 for(r=0; r<=j; r++){
+			 temp = N[r] / (right[r + 1] + left[j - r]);
+			 N[r] = saved + right[r + 1] * temp;
+			 saved = left[j - r] * temp;
+		 }
+		 
+		 N[j] = saved;
+		 
+	 }
+	 
+	 return N;
+ }
