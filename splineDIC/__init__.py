27a6eac8d567cfa86af6c41452a4fbe60283e7e8
Sam Potter <spotter1642@gmail.com>
2019-05-27 14:14:08 -0500

    removing old dependencies from init file

--- splineDIC/__init__.py
+++ splineDIC/__init__.py
@@ -18,9 +18,6 @@ import errno
 import regex
 from scipy import signal
 import warnings
-import geomdl.helpers as helpers
-import geomdl.BSpline as bs
-import geomdl.utilities as gutil
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 from scipy.ndimage import sobel
 from numba import jit
