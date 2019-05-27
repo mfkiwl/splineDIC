b4365dc1e7ae0ba95bba7820349905a43761aa50
Sam Potter <spotter1642@gmail.com>
2019-05-27 14:56:40 -0500

    adding surface read/write to/from text functions [ci-skip]

--- splineDIC/fileIO.py
+++ splineDIC/fileIO.py
@@ -152,3 +152,101 @@ def write_tiff_stack(data_array, base_name, directory=None, color_map='viridis')
     if directory:
         # Move directory back to start
         os.chdir(start)
+
+
+def write_surface_to_txt(surface_instance, filename):
+    """
+    Save Spline Surface parametrization data to text file.
+
+    Saves degree, knot vector, and control points.
+
+    :param surface_instance: Surface object to save
+    :type surface_instance: spline.Surface() object
+    :param filename: Path (may be relative or absolute) to destination file. Must include extension.
+    :type filename: str
+    :return: None
+    :rtype: None
+    """
+    with open(filename, 'w') as f:
+        # Write degree information
+        f.write('Degree - p:\n')
+        f.write('{}\n'.format(surface_instance.degree_u))
+
+        f.write('Degree - q:\n')
+        f.write('{}\n'.format(surface_instance.degree_v))
+
+        # Write knot vector information
+        f.write('Number of Knots in U\n')
+        f.write('{}\n'.format(len(surface_instance.knot_vector_u)))
+
+        f.write('Number of Knots in V\n')
+        f.write('{}\n'.format(len(surface_instance.knot_vector_v)))
+
+        f.write('Knot Vector - U\n')
+        for knot in surface_instance.knot_vector_u:
+            f.write('{}\t'.format(knot))
+        f.write('\n')
+
+        f.write('Knot Vector - V\n')
+        for knot in surface_instance.knot_vector_v:
+            f.write('{}\t'.format(knot))
+        f.write('\n')
+
+        # Write control points info
+        f.write('Number of Control Points in U\n')
+        f.write('{}\n'.format(surface_instance.num_ctrlpts_u))
+
+        f.write('Number of Control Points in V\n')
+        f.write('{}\n'.format(surface_instance.num_ctrlpts_v))
+
+        f.write('Control Points\n')
+        for ctrlpt in surface_instance.control_points:
+            f.write('{}\t {}\t {}\n'.format(ctrlpt[0], ctrlpt[1], ctrlpt[2]))
+
+
+def read_surf_from_txt(surface_instance, filename):
+    """
+    Read spline surface data from file and modify an instance of the spline.Surface() class
+
+    :param surface_instance: Surface object to define with data in filename
+    :type surface_instance: spline.Surface() object
+    :param filename: Path (relative or absolute) to file containing curve data. Must include extension.
+    :type filename: str
+    :return: None
+    :rtype: None
+    """
+
+    with open(filename, 'r') as f:
+        contents = [line.strip('\n') for line in f]
+
+        # Pull degree
+        degree_u = int(contents[1])
+        degree_v = int(contents[3])
+
+        # Get number of knots
+        num_knots_u = int(contents[5])
+        num_knots_v = int(contents[7])
+
+        # Get knot values
+        knot_vector_u = np.array(list(map(float, contents[9].split('\t'))))
+        knot_vector_v = np.array(list(map(float, contents[11].split('\t'))))
+
+        # Get number of control points
+        num_ctrlpts_u = int(contents[13])
+        num_ctrlpts_v = int(contents[15])
+
+        # Get actual control points
+        control_points = [list(map(float, contents[line].split())) for line in range(17, len(contents))]
+        control_points = np.array(control_points)
+
+        # Setup the curve
+        surface_instance.degree_u = degree_u
+        surface_instance.degree_v = degree_v
+
+        surface_instance.num_ctrlpts_u = num_ctrlpts_u
+        surface_instance.num_ctrlpts_v = num_ctrlpts_v
+
+        surface_instance.control_points = control_points
+
+        surface_instance.knot_vector_u = knot_vector_u
+        surface_instance.knot_vector_v = knot_vector_v
