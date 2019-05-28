2f19d576ea33d340debea12a46549bef03d566d5
Sam Potter <spotter1642@gmail.com>
2019-05-28 09:09:23 -0500

    adding getter/setter for image data type

--- splineDIC/image.py
+++ splineDIC/image.py
@@ -6,7 +6,8 @@
 .. moduleauthor:: Sam Potter <spotter1642@gmail.com>
 """
 
-from . import np
+from . import os
+from . import cv2
 
 
 class Image:
@@ -15,15 +16,48 @@ class Image:
     Data storage and evaluation class for DIC image data
 
     Attributes
-    image_data: Ndarray. Discrete image data values at each pixel level. Shape Size X x Size Y.
     data_type: str. String describing type of image data. Options: ['DIC', 'phi', 'DOA']
+    image_data: Ndarray. Discrete image data values at each pixel level. Shape Size X x Size Y.
 
     Methods
     """
 
-    def __init__(self, image_data, data_type):
-        self._image_data = image_data
-        self._data_type = data_type
+    def __init__(self):
+        self._image_data = None
+        self._data_type = None
+
+    @property
+    def data_type(self):
+        """
+        Image data type
+        One of these strings: ['DIC", 'phi', 'DOA']
+
+        :getter: Get image data type
+        :setter: Set image data type
+        :type: str
+        """
+        return self._data_type
+
+    @data_type.setter
+    def data_type(self, imtype):
+        """
+        Set image data type
+
+        :param imtype: One of these strings: ['DIC", 'phi', 'DOA']
+        :type imtype: str
+        :return: None
+        :rtype: None
+        """
+        # Input checking
+        if not isinstance(imtype, str):
+            raise TypeError('Image data type must be a string')
+
+        accepted_vals = ['DIC', 'phi', 'DOA']
+
+        if imtype not in accepted_vals:
+            raise ValueError('Image data type must be one of these: {}'.format(accepted_vals))
+
+        self._data_type = imtype
 
     # Data property definition
     @property
@@ -56,3 +90,24 @@ class Image:
 
         # Set _data property
         self._image_data = array
+
+    def load(self, filename):
+        """
+        Sets image data by loading from an image file
+
+        :param filename: Path to file including extension. Absolute or relative
+        :type filename: str
+        :return: None
+        :rtype: None
+        """
+
+        # Input checking
+        if not isinstance(filename, str):
+            raise TypeError('Filename must be a string')
+
+        if not os.path.isfile(filename):
+            raise ValueError('Filename is not a valid path')
+
+        imdata = cv2.imread(filename, -1)  # Read as is
+
+        self.image_data(imdata)
