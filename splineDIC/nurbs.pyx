46c7bd71d7dfd85c4d587e7bfe32d18d5a1daee5
Sam Potter <spotter1642@gmail.com>
2019-06-04 10:51:49 -0500

    adding surface point extension function

--- splineDIC/nurbs.pyx
+++ splineDIC/nurbs.pyx
@@ -36,3 +36,28 @@ def basis_functions(N, knot_span, knot, degree, knot_vector):
 
     if error != 0:
         print('Error calculating basis functions')
+
+cdef extern from "../src/cnurbs.h":
+
+    double surface_pointC(unsigned int, unsigned int, double *, unsigned int, unsigned int, double *, double *, double, double)
+
+def surface_point(ncpts_u, deg_u, kv_u, ncpts_v, deg_v, kv_v, ctrlpts, u, v):
+
+    if not kv_u.flags['C_CONTIGUOUS']:
+        kv_u = np.ascontiguousarray(kv_u)
+
+    if not kv_v.flags['C_CONTIGUOUS']:
+        kv_v = np.ascontiguousarray(kv_v)
+
+    if not ctrlpts.flags['C_CONTIGUOUS']:
+        ctrlpts = np.ascontiguousarray(ctrlpts)
+
+    cdef double[::1] kv_u_memview = kv_u
+
+    cdef double[::1] kv_v_memview = kv_v
+
+    cdef double[::1] ctrlpts_memview = ctrlpts
+
+    s = surface_pointC(ncpts_u, deg_u, &kv_u_memview[0], ncpts_v, deg_v, &kv_v_memview[0], &ctrlpts_memview[0], u, v)
+
+    return s
