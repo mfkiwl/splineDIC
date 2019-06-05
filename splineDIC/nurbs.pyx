5dbb1a91484f8bf22d58b35bb6f9be8faa31c401
Sam Potter <spotter1642@gmail.com>
2019-06-05 11:24:41 -0500

    adding surface points function to cython file

--- splineDIC/nurbs.pyx
+++ splineDIC/nurbs.pyx
@@ -61,3 +61,44 @@ def surface_point(ncpts_u, deg_u, kv_u, ncpts_v, deg_v, kv_v, ctrlpts, u, v):
     s = surface_pointC(ncpts_u, deg_u, &kv_u_memview[0], ncpts_v, deg_v, &kv_v_memview[0], &ctrlpts_memview[0], u, v)
 
     return s
+
+cdef extern from "../src/cnurbs.h":
+
+    int surface_pointsC(double *, unsigned int, unsigned int, double *, unsigned int, unsigned int, double *, double *, double*, double*, unsigned int)
+
+def surface_points(S, ncpts_u, deg_u, kv_u, ncpts_v, deg_v, kv_v, ctrlpts, u, v, npts):
+
+    if not S.flags['C_CONTIGUOUS']:
+        S = np.ascontiguousarray(S)
+
+    if not kv_u.flags['C_CONTIGUOUS']:
+        kv_u = np.ascontiguousarray(kv_u)
+
+    if not kv_v.flags['C_CONTIGUOUS']:
+        kv_v = np.ascontiguousarray(kv_v)
+
+    if not ctrlpts.flags['C_CONTIGUOUS']:
+        ctrlpts = np.ascontiguousarray(ctrlpts)
+
+    if not u.flags['C_CONTIGUOUS']:
+        u = np.ascontiguousarray(u)
+
+    if not v.flags['C_CONTIGUOUS']:
+        v = np.ascontiguousarray(v)
+
+    cdef double[::1] S_memview = S
+
+    cdef double[::1] kv_u_memview = kv_u
+
+    cdef double[::1] kv_v_memview = kv_v
+
+    cdef double[::1] ctrlpts_memview = ctrlpts
+
+    cdef double[::1] u_memview = u
+
+    cdef double[::1] v_memview = v
+
+    error = surface_pointsC(&S_memview[0], ncpts_u, deg_u, &kv_u_memview[0], ncpts_v, deg_v, &kv_v_memview[0], &ctrlpts_memview[0], &u_memview[0], &v_memview[0], npts)
+
+    if error != 0:
+        print('Something went wrong computing the surface points!')
